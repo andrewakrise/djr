265cd9044b0738dccd3013ca81990ec63f59e590
andreyrise <andrey.rise@gmail.com>
2023-05-17 19:38:00 -0700

    prepare to deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
         >
           click here to listening
         </a>
+        <div>
+          <iframe width="100%" height="166" allow="autoplay"
+            src="https://soundcloud.com/andrew_rise/podcast-247-dj-rise-2904?si=e47af6d3abd0496dbaa385a4f8071fdf&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+          />
+        </div>
       </header>
       <main>
       <div className="app-link">
