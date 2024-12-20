ae7fd67e3d8dff0a8d19f2b0c15f56ee966eeee6
andrewkukhar <andriikukharv@gmail.com>
2024-12-19 16:03:08 -0800

    added new route redirection

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use((req, res, next) => {
+  if (req.hostname === "www.djsrise.com") {
+    return res.redirect(301, `https://www.djrise.party${req.originalUrl}`);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
@@ -9,6 +16,6 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`RISE DJ Frontend server listening on ${port}`);
+app.listen(port, () => {
+  console.log(`RISE DJ Frontend server listening on ${port}`);
+});
